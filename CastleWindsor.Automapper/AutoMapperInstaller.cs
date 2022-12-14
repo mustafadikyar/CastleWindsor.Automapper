10306d12307cea6771f0d9f38b58da2e5f6e8493
Mustafa Dikyar <mustafa.dikyar@setur.com.tr>
2022-12-14 16:13:38 +0300

    Add project files.

---
+++ CastleWindsor.Automapper/AutoMapperInstaller.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace CastleWindsor.Automapper
+{
+    public class AutoMapperInstaller : IWindsorInstaller
+    {      
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IMapper>().UsingFactoryMethod(x =>
+            {
+                return new MapperConfiguration(c =>
+                {
+                    foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.StartsWith("YourPath")))
+                    {
+                        var profiles = from t in assembly.GetTypes()
+                                       where typeof(Profile).IsAssignableFrom(t) && t.Assembly != typeof(Profile).Assembly
+                                       select Activator.CreateInstance(t) as Profile;
+
+                        foreach (var profile in profiles)
+                        {
+                            c.AddProfile(profile);
+                        }
+                    }
+                }).CreateMapper();
+            }));
+        }
+    }
+}
